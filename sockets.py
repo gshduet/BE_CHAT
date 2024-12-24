527d88f5fd3b5f4118016dd6fc49fcb8130d388e
ddoavoca <ddoavoca@gmail.com>
2024-12-24 16:15:49 +0900

    241224 feat: sio 채팅 기능 구현
    각 클라이언트를 방에 분류하기 위한 데이터 구조 추가.
    CS_CHAT 이벤트를 처리하여 메시지를 해당 방의 클라이언트에게만 전송하도록 로직 수정.
    브로드캐스트와 방 변경 관련 코드 추가(이후 API 서버에서 처리할 예정).
    sid는 이후 유저의 아이디로 대체 예정.

--- sockets.py
+++ sockets.py
@@ -10,18 +10,67 @@ sio_app = socketio.ASGIApp(
     socketio_path='sockets'
 )
 
+# 방 정보를 관리할 딕셔너리
+# {"room_id": str, "clients": list[{clients_id, img_url}]}
+rooms = {}
+rooms['floor07'] = []
+rooms['m'] = []
+
+# 클라이언트 정보를 관리할 딕셔너리
+# {"client_id": {"img_url": str, "room_id": str}}
+clients = {}
 
 @sio_server.event
-async def connect(sid, environ, auth):
+async def connect(sid, environ):
     print(f'{sid}: connected')
+    # 클라이언트가 connect하면 기본 룸 floor07에 join
+    rooms['floor07'].append({'client_id': sid, 'img_url': "https://i.imgur"})
+    clients[sid] = {'img_url': "https://i.imgur", 'room_id': 'floor07'}
+    print(rooms)
+    print(f'{sid}: joined floor07')
+    
     await sio_server.emit('join', {'sid': sid})
 
 
 @sio_server.event
-async def chat(sid, message):
-    await sio_server.emit('chat', {'sid': sid, 'message': message})
+async def CS_CHAT(sid, data):
+    message = data.get('message')
+
+    # 클라이언트가 m이라는 메세지를 보내면 해당 클라이언트의 rooms 정보를 m으로 변경
+    # 이후에 이 부분은 api 서버에서 처리할 예정
+    if message == 'm':
+        room_id = clients[sid]['room_id']
+        rooms[room_id] = [
+            client for client in rooms[room_id] 
+            if client['client_id'] != sid
+        ]
+        clients[sid]['room_id'] = 'm'
+        rooms['m'].append({'client_id': sid, 'img_url': clients[sid]['img_url']})
+        print(rooms)
+
+        # 디버깅용 코드
+        await sio_server.emit('SC_CHAT', 
+                              {'sid': sid, 'message': "join m room"}, 
+                              to=sid)
+
+    else:
+        # 이 클라이언트의 room 정보를 가져옴
+        room = clients[sid]['room_id']
+        # 해당 room에 있는 모든 클라이언트에게 메세지를 보냄
+        for client in rooms[room]:
+            await sio_server.emit('SC_CHAT', 
+                                  {'sid': sid, 
+                                   'message': message}, 
+                                  to=client['client_id'])
 
 
 @sio_server.event
 async def disconnect(sid):
+    # 클라이언트가 disconnect하면 해당 클라이언트의 room에서 제거
+    room_id = clients[sid]['room_id']
+    rooms[room_id] = [
+        client for client in rooms[room_id] 
+        if client['client_id'] != sid
+    ]
+    clients.pop(sid, None)
     print(f'{sid}: disconnected')
