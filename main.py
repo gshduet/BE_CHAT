677b7ee7387699fb1acc16c3c446e07627cf55a7
ddoavoca <ddoavoca@gmail.com>
2025-01-01 18:22:35 +0900

    250101 refactor: CORS 및 Allow Origins 설정 개선
    CORS 설정과 Allow Origins를 특정 도메인 허용으로 수정.
    - `cors_allowed_origins`와 `allow_origins`를 `https://jgtower.com`으로 변경하여 자체 도메인만 허용.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.mount("/sio", app=sio_app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://jgtower.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
